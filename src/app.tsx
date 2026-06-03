4364ab662b343656b7cbf8bf559e45c282656fb6
Lazuardy Anugrah <lazuardyanugrah@gmail.com>
2026-06-03 12:23:47 +0700

    create task-card.tsx component file

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,10 @@
+import { TaskItem } from "./task-card";
+
 export function App() {
   return (
     <div>
       <h1>Task Management</h1>
       <TaskItem />
-    </div>
-  );
-}
-
-function TaskItem() {
-  return (
-    <div>
-      <h2>Work 1</h2>
-      <p>Submit document</p>
     </div>
   );
 }
---
+++ src/task-card.tsx
@@ -0,0 +1,8 @@
+export function TaskItem() {
+  return (
+    <div>
+      <h2>Work 1</h2>
+      <p>Submit document</p>
+    </div>
+  );
+}
