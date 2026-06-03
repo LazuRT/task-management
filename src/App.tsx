ab355ce0ae79fc312a6ba881be7fe9e8d73b2e43
Lazuardy Anugrah <lazuardyanugrah@gmail.com>
2026-06-03 12:20:12 +0700

    add taskitem component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ export function App() {
   return (
     <div>
       <h1>Task Management</h1>
+      <TaskItem />
+    </div>
+  );
+}
+
+function TaskItem() {
+  return (
+    <div>
+      <h2>Work 1</h2>
+      <p>Submit document</p>
     </div>
   );
 }
