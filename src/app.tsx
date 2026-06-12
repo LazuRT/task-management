dd464896005a69d234680b02e93040bba6342e09
Lazuardy Anugrah <lazuardyanugrah@gmail.com>
2026-06-12 18:58:01 +0700

    create add-list component

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,14 @@
-import { Button } from "./components/ui/button";
 import { TaskList } from "./modules/task-list/components/task-list";
 import { taskLists } from "./modules/task-list/task-list.data";
+import { AddList } from "./modules/task-list/components/add-list";
 
 export function App() {
   return (
     <div className="mx-auto max-w-lg px-4 py-8">
       <h1 className="text-3xl font-bold">Task Management</h1>
 
-      {/* <section>
-        <Button>Test Button</Button>
-        <Button variant={"outline"}>Test Button</Button>
-      </section> */}
+      <AddList />
+
       <ul className="flex flex-col gap-4 p-4">
         {taskLists.map((list) => (
           <li>
---
+++ src/modules/task-list/components/add-list.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Field, FieldGroup } from "@/components/ui/field";
+
+export function AddList() {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger
+          render={<Button variant="outline">Create New List</Button>}
+        />
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Create New List</DialogTitle>
+            <DialogDescription>
+              Input your new task list name.
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="name-1">List Name</Label>
+              <Input id="name-1" name="name" placeholder="Work List" />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose render={<Button variant="outline">Cancel</Button>} />
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
