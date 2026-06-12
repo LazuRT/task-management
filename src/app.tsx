5758a5934de21e1a43e7882f255234298488611e
Lazuardy Anugrah <lazuardyanugrah@gmail.com>
2026-06-12 14:27:27 +0700

    setting shadcnui

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button";
 import { TaskList } from "./modules/task-list/components/task-list";
 import { taskLists } from "./modules/task-list/task-list.data";
 
@@ -5,6 +6,11 @@ export function App() {
   return (
     <div>
       <h1 className="text-5xl font-bold">Task Management</h1>
+
+      <section>
+        <Button>Test Button</Button>
+        <Button variant={"outline"}>Test Button</Button>
+      </section>
 
       {taskLists.map((list) => (
         <TaskList key={list.id} list={list} />
