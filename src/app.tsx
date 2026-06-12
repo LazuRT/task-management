75547f5271527f9ad56d0ebff5bb70712437c68f
Lazuardy Anugrah <lazuardyanugrah@gmail.com>
2026-06-12 20:01:37 +0700

    styling title and new list button

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,8 @@ import { AddList } from "./modules/task-list/components/add-list";
 
 export function App() {
   return (
-    <div className="mx-auto max-w-lg px-4 py-8">
-      <h1 className="text-3xl font-bold">Task Management</h1>
+    <div className="mx-auto max-w-xl px-4 py-8">
+      <h1 className="text-center text-3xl font-bold">Task Management</h1>
 
       <AddList />
 
--- src/modules/task-list/components/add-list.tsx
+++ src/modules/task-list/components/add-list.tsx
@@ -15,30 +15,32 @@ import { Field, FieldGroup } from "@/components/ui/field";
 
 export function AddList() {
   return (
-    <Dialog>
-      <form>
-        <DialogTrigger
-          render={<Button variant="outline">Create New List</Button>}
-        />
-        <DialogContent className="sm:max-w-md">
-          <DialogHeader>
-            <DialogTitle>Create New List</DialogTitle>
-            <DialogDescription>
-              Input your new task list name.
-            </DialogDescription>
-          </DialogHeader>
-          <FieldGroup>
-            <Field>
-              <Label htmlFor="name-1">List Name</Label>
-              <Input id="name-1" name="name" placeholder="Work List" />
-            </Field>
-          </FieldGroup>
-          <DialogFooter>
-            <DialogClose render={<Button variant="outline">Cancel</Button>} />
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
-    </Dialog>
+    <div className="flex items-center justify-center py-2">
+      <Dialog>
+        <form>
+          <DialogTrigger
+            render={<Button variant="outline">Create New List</Button>}
+          />
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>Create New List</DialogTitle>
+              <DialogDescription>
+                Input your new task list name.
+              </DialogDescription>
+            </DialogHeader>
+            <FieldGroup>
+              <Field>
+                <Label htmlFor="name-1">List Name</Label>
+                <Input id="name-1" name="name" placeholder="Work List" />
+              </Field>
+            </FieldGroup>
+            <DialogFooter>
+              <DialogClose render={<Button variant="outline">Cancel</Button>} />
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
+    </div>
   );
 }
